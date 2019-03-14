4f7a063d5220692100a4c7125a2c2b38e230929a
Evan Downing <2077950+evandowning@users.noreply.github.com>
2019-03-14 20:03:11 +0000

    add wget to installation

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ set -e
 apt update
 
 # Install neo4j - https://debian.neo4j.org/
+apt install -y wget
 wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
 apt update
