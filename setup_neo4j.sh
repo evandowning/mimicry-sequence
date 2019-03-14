6a944c1dad0329d5b447f913a3971ec6a3f7b35b
Evan Downing <2077950+evandowning@users.noreply.github.com>
2019-03-14 22:45:25 +0000

    update setup neo4j

--- setup_neo4j.sh
+++ setup_neo4j.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" -X POST -d '{"password":"password"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+neo4j-admin set-initial-password password
+service neo4j restart
